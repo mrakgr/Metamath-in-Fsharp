0554e17c3ecff08f53175e72aad4b482e811bcf2
Marko Grdinić <mrakgr@gmail.com>
2019-12-01 12:39:13 +0100

    ...

--- Metamath/Script4.fsx
+++ Metamath/Script4.fsx
@@ -1,34 +1,68 @@
-﻿let substitute (desc : string list) (m : Map<string, string list>) =
-    List.collect (fun x -> 
+﻿open System.Collections.Generic
+
+let disjointness_check (disjoint_vars : Map<string, Set<string>>) (substs_map : Dictionary<string, Set<string>>) =
+    let fails = ResizeArray()
+
+    let check (kv : KeyValuePair<_,_>) next prev = 
+        let var', subvars' as cur = kv.Key, kv.Value
+        match prev with
+        | None -> ()
+        | Some (var, subvars) as prev ->
+            Set.iter (fun v ->
+                let v_disjoint_vars =
+                    match Map.tryFind v disjoint_vars with
+                    | None -> Set.empty
+                    | Some v_disjoint_vars -> v_disjoint_vars
+                if Set.isEmpty (subvars' - v_disjoint_vars) = false then fails.Add((var,v),(var',subvars'))
+                ) subvars
+            next prev
+        next (Some cur)
+
+    Seq.foldBack check substs_map (fun _ -> ()) None
+    fails.ToArray()
+
+type MMEval =
+    | MMFloating of ty : string * var : string
+    | MMEssential of ty : string * prog : string list * disjoint : Map<string, Set<string>>
+    | MMAxiom of ty : string * prog : string list * disjoint : Map<string, Set<string>>
+
+let substitute is_var (m : Map<string, string list>) (desc : string list) =
+    let subvars = Dictionary(HashIdentity.Structural)
+    subvars,
+    List.collect (fun x ->
         match Map.tryFind x m with
-        | Some x -> x
+        | Some v ->
+            match subvars.TryGetValue x with
+            | false, _ -> subvars.[x] <- List.fold (fun s x -> if is_var x then Set.add x s else s) Set.empty v
+            | true, _ -> ()
+            v
         | None -> [x]
         ) desc
 
-let disjointness_check (disjoint_vars : Map<string, Set<string>>) (substs_map : Map<string,Set<string>>) =
-    let fails = ResizeArray()
-
-    let find x m on_succ = 
-        match Map.tryFind x m with
-        | None -> ()
-        | Some a -> on_succ a
-
-    /// Find all the outer substitutions that need to be checked.
-    Map.iter (fun var subvars ->
-        find var disjoint_vars (
-            Set.iter (fun var' ->
-                find var' substs_map (fun subvars' ->
-                    // Make sure that the inner substitutions make sense.
-                    Set.iter (fun v ->
-                        let v_disjoint_vars = 
-                            match Map.tryFind v disjoint_vars with
-                            | None -> Set.singleton v
-                            | Some v_disjoint_vars -> Set.add v v_disjoint_vars
-                        if Set.isEmpty (subvars' - v_disjoint_vars) = false then fails.Add(var,var',v,subvars')
-                        ) subvars
-                    )
-                )
-            )
-        ) substs_map
-
-    fails.ToArray()
+let prove (floating : Map<string, string>) (x : (MMEval * (string * string list)) list) = 
+    List.fold (fun m x ->
+        match x with
+        | MMFloating(ty,var), (ty', var') ->
+            if ty = ty' then Map.add var var' m
+            else failwith "ty <> ty'"
+        | MMEssential(ty,prog,disjoint), (ty',prog') ->
+            if ty = ty' then 
+                let used_vars, prog'' = substitute (fun x -> Map.containsKey x floating) m prog
+                if prog' = prog'' then
+                    match disjointness_check disjoint used_vars with
+                    | [||] -> m
+                    | errors -> 
+                        Array.map (fun ((var,v),(var',subvars')) -> sprintf "In the substitution of %s and %s, the variable %s is not disjoint from %A" var var' v subvars') errors
+                        |> String.concat "\n"
+                        |> failwith
+                else failwith "unification failed"
+            else failwith "ty <> ty'"
+        | MMAxiom(_,prog,disjoint), _ ->
+            let used_vars, prog'' = substitute (fun x -> Map.containsKey x floating) m prog
+            match disjointness_check disjoint used_vars with
+            | [||] -> m // should return prog''
+            | errors -> 
+                Array.map (fun ((var,v),(var',subvars')) -> sprintf "In the substitution of %s and %s, the variable %s is not disjoint from %A" var var' v subvars') errors
+                |> String.concat "\n"
+                |> failwith
+        ) Map.empty x
---
+++ Metamath/Script5.fsx
@@ -0,0 +1,197 @@
+﻿open System.Collections.Generic
+
+#if INTERACTIVE
+#r @"..\packages\FParsec.1.1.0-RC\lib\net45\FParsecCS.dll"
+#r @"..\packages\FParsec.1.1.0-RC\lib\net45\FParsec.dll"
+#endif
+open FParsec
+
+type Label = int
+type Disjoint = Map<string, Set<string>>
+type Floating = Map<string,Label>
+type EssentialLabels = Set<Label>
+type FloatingLabels = Set<Label>
+type SymbolSequence = string []
+
+type State = { 
+    // Local
+    vars : Set<string>
+    disjoint : Disjoint
+    floating : Floating
+    essential : EssentialLabels
+    labels_in_scope : Set<Label>
+    // Global
+    cons : HashSet<string>
+    label_tag : Dictionary<string,Label>
+    label_floating : Dictionary<Label,string * string>
+    label_esential : Dictionary<Label,string * SymbolSequence * Disjoint * FloatingLabels>
+    label_axiom : Dictionary<Label,string * SymbolSequence * Disjoint * FloatingLabels * EssentialLabels>
+    label_proof : Dictionary<Label,string * SymbolSequence * Disjoint * FloatingLabels * EssentialLabels>
+    }
+
+let tag (x : State) v = x.label_tag.[v]
+
+let many_array_first_elem x0 = let ra = ResizeArray<_>() in ra.Add(x0); ra
+let many_array_fold_state (ra : ResizeArray<_>) x = ra.Add(x); ra
+let many_array_result_from_state (ra : ResizeArray<_>) = ra.ToArray()
+let many1_array p s =
+    Inline.Many(elementParser = p, stateFromFirstElement = many_array_first_elem, foldState = many_array_fold_state,
+                resultFromState = many_array_result_from_state) s
+let many_array p s =
+    Inline.Many(elementParser = p, stateFromFirstElement = many_array_first_elem, foldState = many_array_fold_state,
+                resultFromState = many_array_result_from_state, resultForEmptySequence = (fun () -> [||])) s
+
+let is_label c = isAsciiLetter c || isDigit c || c = '-' || c = '_' || c = '.'
+let is_math_symbol c = '!' <= c && c <= '~' && c <> '$'
+
+let label s = (many1SatisfyL is_label "Ascii letter, digit, `-`, `_` or `.`") s
+let math_symbol s = (many1SatisfyL is_math_symbol "Ascii character between `!` and `~` (excluding `$`.)") s
+
+let terminal x s = (skipString x >>. (spaces1 <|> eof)) s
+let skip_string x s = (skipString x >>. spaces1) s 
+
+let inline er x = Reply(Error, messageError x)
+let inline proceed_if_active check x (s : CharStream<_>) = 
+    if check x s.UserState then Reply(x)
+    else er (sprintf "%s must be active." x)
+
+let add_if_inactive get set x' (s : CharStream<_>) =
+    let u = s.UserState
+    let x = tag u x'
+    let a = get u
+    if Set.contains x a then s.UserState <- set u (Set.add x a); Reply(x)
+    else er (sprintf "%s is already active." x')
+
+let var s = 
+    (math_symbol >>= fun x s -> 
+        let u = s.UserState
+        let a = u.vars
+        if Set.contains x a then s.UserState <- {u with vars=Set.add x a}; Reply(x)
+        else er (sprintf "Variable %s is already active." x)
+        ) s
+
+let con s = 
+    (math_symbol >>= fun x s -> 
+        let u = s.UserState
+        let a = u.cons
+        if a.Contains x then a.Add x |> ignore; Reply(x)
+        else er (sprintf "Constant %s is already active." x)
+        ) s
+
+let vars s = (between (skip_string "$v") (terminal "$.") (skipMany (var >>. spaces1))) s
+let cons s = (between (skip_string "$c") (terminal "$.") (skipMany (con >>. spaces1))) s
+
+//let is_label_inactive x u = (Set.contains x u.labels_hypothesis || Set.contains x u.labels_assertion || Set.contains x u.vars || Set.contains x u.cons) = false
+
+let active_constant s = (math_symbol >>= proceed_if_active (fun x u -> u.cons.Contains x)) s
+let active_variable s = (math_symbol >>= proceed_if_active (fun x u -> Set.contains x u.vars)) s
+let floating_math_symbol s = (math_symbol >>= proceed_if_active (fun x u -> Map.containsKey x u.floating || u.cons.Contains x)) s
+
+let er_label_is_active label = er (sprintf "Label %s is already active." label)
+let check_floating var (s : CharStream<_>) =
+    if Map.containsKey var s.UserState.floating then Reply(var)
+    else er (sprintf "There may not be two active $f statements containing the same variable %s." var)
+
+let floating label tag s = 
+    (
+    skip_string "$f" >>.
+    tuple2 (active_constant .>> spaces1) (active_variable >>= check_floating .>> spaces1 .>> terminal "$.") 
+    >>= fun (con, var) -> 
+        updateUserState (fun u ->
+            u.label_floating.[tag] <- (con, var)
+            {u with floating=Map.add label tag u.floating}
+            )
+    ) s
+
+let floating_labels m ar =
+    Array.fold (fun s x ->
+        match Map.tryFind x m with
+        | Some t -> Set.add t s
+        | None -> s
+        ) Set.empty ar
+
+let modifyUserState f (s: CharStream<_>) = f s.UserState; Reply(())
+
+let essential label tag s =
+    (
+    skip_string "$e" >>.
+    tuple2 (active_constant .>> spaces1) (many_array (floating_math_symbol .>> spaces1) .>> terminal "$.") 
+    >>= fun (c, v) -> updateUserState (fun u -> 
+            u.label_esential.[tag] <- (c,v,u.disjoint,floating_labels u.floating v)
+            {u with essential=Set.add tag u.essential}
+        )
+    ) s
+
+let disjoint s = 
+    let check x msg _ = if x then Reply(()) else er msg
+    let h = HashSet(HashIdentity.Structural)
+    (
+    between (skip_string "$d") (terminal "$.") 
+        (skipMany (active_variable >>= (fun x -> check (h.Add(x)) (sprintf "%s cannot be disjoint with itself." x)) >>. spaces1))
+    >>= (fun _ -> check (h.Count >= 2) "The $d statement needs at least two variables")
+    >>= fun _ -> updateUserState (fun u ->
+        {u with disjoint=
+                let f x' next x disjoint =
+                    match x with
+                    | None -> next (Some x') disjoint
+                    | Some x as prev ->
+                        let add x x' m =
+                            match Map.tryFind x m with
+                            | Some d -> Map.add x (Set.add x' d) m
+                            | None -> Map.add x (Set.singleton x') m
+                        disjoint |> add x x' |> add x' x |> next prev |> next (Some x')
+
+                Seq.foldBack f h (fun _ x -> x) None u.disjoint
+            }
+        )
+    ) s
+        
+
+let axiom label tag s = 
+    (
+    skip_string "$a" >>.
+    tuple2 (active_constant .>> spaces1) (many_array (floating_math_symbol .>> spaces1))
+    >>= fun (c,v) -> modifyUserState (fun u -> u.label_axiom.[tag] <- (c,v,u.disjoint,floating_labels u.floating v,u.essential))
+    >>. terminal "$." 
+    ) s
+
+let active_label s = 
+    (label >>= proceed_if_active (fun x u -> 
+        match u.label_tag.TryGetValue x with 
+        false, _ -> false 
+        | true, v -> Set.contains v u.labels_in_scope || u.label_axiom.ContainsKey v)
+        ) s
+
+let proof label s = 
+    add_label Wrap Assertion (
+        skip_string "$p" >>.
+        pipe3 (active_constant .>> spaces1) 
+            (many_array (floating_math_symbol .>> spaces1) .>> skip_string "$=") 
+            (many_array (active_label .>> spaces1) .>> terminal "$.") (fun con syms labels -> ExprP(label,con,syms,labels))
+        ) label s
+
+let block next s =
+    let f next (s : CharStream<_>) = 
+        let u = s.UserState
+        let r = next s
+        s.UserState <- {s.UserState with vars=u.vars; vars_floating=u.vars_floating; labels_hypothesis=u.labels_hypothesis}
+        r
+    between (skip_string "${") (terminal "$}") (f (many_array next) |>> ExprBlock) s
+
+let comment s = 
+    let rec body s = (charsTillString "$" true System.Int32.MaxValue >>. (skipChar ')' <|> (skipChar '(' >>. fail "Nested comments are not allowed.") <|> body)) s
+    (skipString "$(" >>. body >>. (spaces1 <|> eof)) s
+
+let proceed_if_inactive_label label (s : CharStream<_>) =
+    if is_label_inactive label s.UserState then Reply(label)
+    else er_label_is_active label
+let checked_label s = (label >>= proceed_if_inactive_label) s
+
+let parser s =
+    let rec body s = 
+        choice [|
+            checked_label >>= fun label -> spaces1 >>. choice [|floating label; essential label; axiom label; proof label|]
+            vars; disjoint; comment >>. body; block body; file_include
+            |] s
+
+    (many_array (cons <|> body) |>> ExprBlock .>> eof) s
