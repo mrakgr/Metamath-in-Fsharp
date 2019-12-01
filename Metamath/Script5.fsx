e74743476dc7c75bd1424510a2ab7e0cca7fb4c4
Marko Grdinić <mrakgr@gmail.com>
2019-12-01 19:08:52 +0100

    Done for the day here.

--- Metamath/Script5.fsx
+++ Metamath/Script5.fsx
@@ -9,27 +9,29 @@ open FParsec
 type Label = int
 type Disjoint = Map<string, Set<string>>
 type Floating = Map<string,Label>
-type EssentialLabels = Set<Label>
-type FloatingLabels = Set<Label>
+type Labels = Set<Label>
 type SymbolSequence = string []
+
+type Statement =
+    | Floating of string * string
+    | Essential of string * SymbolSequence * Disjoint * Labels
+    | Axiom of string * SymbolSequence * Disjoint * Labels 
+    | Proof of string * SymbolSequence * Disjoint * Labels
 
 type State = { 
     // Local
     vars : Set<string>
     disjoint : Disjoint
     floating : Floating
-    essential : EssentialLabels
-    labels_in_scope : Set<Label>
+    essential : Labels
     // Global
     cons : HashSet<string>
     label_tag : Dictionary<string,Label>
-    label_floating : Dictionary<Label,string * string>
-    label_esential : Dictionary<Label,string * SymbolSequence * Disjoint * FloatingLabels>
-    label_axiom : Dictionary<Label,string * SymbolSequence * Disjoint * FloatingLabels * EssentialLabels>
-    label_proof : Dictionary<Label,string * SymbolSequence * Disjoint * FloatingLabels * EssentialLabels>
+    statements : Dictionary<Label,Statement>
     }
 
 let tag (x : State) v = x.label_tag.[v]
+let tag_create (x : State) label = let c = x.label_tag.Count in x.label_tag.Add(label, c); c
 
 let many_array_first_elem x0 = let ra = ResizeArray<_>() in ra.Add(x0); ra
 let many_array_fold_state (ra : ResizeArray<_>) x = ra.Add(x); ra
@@ -92,18 +94,19 @@ let check_floating var (s : CharStream<_>) =
     if Map.containsKey var s.UserState.floating then Reply(var)
     else er (sprintf "There may not be two active $f statements containing the same variable %s." var)
 
-let floating label tag s = 
+let floating label s = 
     (
     skip_string "$f" >>.
     tuple2 (active_constant .>> spaces1) (active_variable >>= check_floating .>> spaces1 .>> terminal "$.") 
     >>= fun (con, var) -> 
         updateUserState (fun u ->
-            u.label_floating.[tag] <- (con, var)
+            let tag = tag_create u label
+            u.statements.Add(tag,Floating(con, var))
             {u with floating=Map.add label tag u.floating}
             )
     ) s
 
-let floating_labels m ar =
+let vars_sequence m ar =
     Array.fold (fun s x ->
         match Map.tryFind x m with
         | Some t -> Set.add t s
@@ -112,13 +115,14 @@ let floating_labels m ar =
 
 let modifyUserState f (s: CharStream<_>) = f s.UserState; Reply(())
 
-let essential label tag s =
+let essential label s =
     (
     skip_string "$e" >>.
     tuple2 (active_constant .>> spaces1) (many_array (floating_math_symbol .>> spaces1) .>> terminal "$.") 
     >>= fun (c, v) -> updateUserState (fun u -> 
-            u.label_esential.[tag] <- (c,v,u.disjoint,floating_labels u.floating v)
-            {u with essential=Set.add tag u.essential}
+        let tag = tag_create u label
+        u.statements.Add(tag,Essential(c,v,u.disjoint,vars_sequence u.floating v))
+        {u with essential=Set.add tag u.essential}
         )
     ) s
 
@@ -147,28 +151,67 @@ let disjoint s =
     ) s
         
 
-let axiom label tag s = 
+let axiom label s = 
     (
     skip_string "$a" >>.
     tuple2 (active_constant .>> spaces1) (many_array (floating_math_symbol .>> spaces1))
-    >>= fun (c,v) -> modifyUserState (fun u -> u.label_axiom.[tag] <- (c,v,u.disjoint,floating_labels u.floating v,u.essential))
+    >>= fun (c,sym_seq) -> modifyUserState (fun u -> 
+        let tag = tag_create u label
+        u.statements.Add(tag,Axiom(c,sym_seq,u.disjoint,vars_sequence u.floating sym_seq + vars_essential u))
+        )
     >>. terminal "$." 
     ) s
 
-let active_label s = 
-    (label >>= proceed_if_active (fun x u -> 
-        match u.label_tag.TryGetValue x with 
-        false, _ -> false 
-        | true, v -> Set.contains v u.labels_in_scope || u.label_axiom.ContainsKey v)
-        ) s
+let active_label s = (label >>= proceed_if_active (fun x u -> u.label_tag.ContainsKey x)) s
+
+let prove_label stack' label (s : CharStream<State>) =
+    let u = s.UserState
+    match u.statements.[u.label_tag.[label]] with
+    | Floating(c,v) -> stack' := (c,[|v|]) :: !stack'; Reply(())
+    | Essential _ -> er "Essential hypotheses are not directly usable."
+    | Axiom(con,sym_seq,disjoint,hyps) | Proof(con,sym_seq,disjoint,hyps) ->
+        let rec loop_unify m stack = function
+            | (Floating(c,v),(c',[|v'|])) :: ops -> if c = c' then loop_unify (Map.add c' v' m) stack ops else er (sprintf "Unification failed. Typecode %s <> %s" c c')
+            | (Essential(c,sym_seq,disjoint,hyps),(c',sym_seq')) :: ops -> 
+                if c = c' then
+                    let sym_seq = substitute m sym_seq
+                    if sym_seq = sym_seq' then disjointness_check disjoint
+                    else er "Unification failed."
+                else er (sprintf "Unification failed. Typecode %s <> %s" c c')
+            | [] -> stack' := (con, substitute m sym_seq') :: stack; Reply(())
+            | _ -> failwith "impossible"
+
+        let rec loop_init ops hyps stack =
+            match hyps, stack with
+            | [], stack -> loop_unify Map.empty stack ops
+            | (h :: h'), (d :: d') -> loop_init ((u.statements.[h],d) :: ops) h' d'
+            | _, [] -> er "Not enough hypotheses on the stack."
+
+        let hyps = Set.fold (fun b a -> a :: b) [] hyps
+        loop_init [] hyps !stack'
+        
 
 let proof label s = 
-    add_label Wrap Assertion (
-        skip_string "$p" >>.
-        pipe3 (active_constant .>> spaces1) 
-            (many_array (floating_math_symbol .>> spaces1) .>> skip_string "$=") 
-            (many_array (active_label .>> spaces1) .>> terminal "$.") (fun con syms labels -> ExprP(label,con,syms,labels))
-        ) label s
+    (
+    skip_string "$p" >>.
+    tuple2 (active_constant .>> spaces1) (many_array (floating_math_symbol .>> spaces1)) 
+    .>> skip_string "$="
+    >>= fun (c,v) -> 
+        let stack = ref []
+        let prove_label = fun label s -> failwith ""
+        let prove_finish s =
+            let concat (c,v) = sprintf "%s %s" c (String.concat " " v)
+            match !stack with
+            | [c',v'] -> if c <> c' || v <> v' then er (sprintf "Unification failed. The expression %s fails to prove the proof %s" (concat (c,v)) (concat (c',v'))) else Reply(())
+            | l -> 
+                List.map concat l
+                |> String.concat "\n"
+                |> sprintf "Unification failed. A single term must be left at the end of the proof. Got:\n%s"
+                |> er
+
+        (skipMany (active_label >>= prove_label >>. spaces1) >>. prove_finish)
+    >>. terminal "$."
+    ) s
 
 let block next s =
     let f next (s : CharStream<_>) = 
